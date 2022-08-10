3820240edb186f6576a6ab580072a5d7fc887282
Sachin rajput <sachin108rajput@hotmail.com>
2022-08-10 23:33:33 +0530

    10th AUG

--- Object_Oriented_Programming/Inheritance/Main.java
+++ Object_Oriented_Programming/Inheritance/Main.java
@@ -21,18 +21,23 @@ class Dog extends Animal{
     String type = "Mammal";
 
     Dog(){
-        super(); // it will invoke super class no parametrized constructor + we can also call paremetrized constructor
+        super(); // it will invoke super class no parametrized constructor
+        // it can automatically calls the default or no-arg constructor even if we don't specify super()
         System.out.println("I am an "+super.type+" of type "+ type);
     }
+    //we can call the constructor of the superclass from its subclasses using super()
 
     void display(){
         System.out.println("My name is "+name); // name inherited from Parent class Animal
     }
-
+    @Override
     void override(){
         super.override(); // super keyword is used to call the method of the parent class from child class.
         System.out.println("It's Dog class");
     }
+    //@Override annotation: In Java, annotations are the metadata .. used to provide information to the compiler. 
+    // not mandatory to use 
+    // We cannot override the method declared as final and static
 }
 
 public class Main{
