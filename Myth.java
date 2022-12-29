ba6c94e9575845c2ef56395d979a49d55d5dfcce
Sachin rajput <sachin108rajput@hotmail.com>
2022-12-29 18:55:03 +0530

    29th Dec

---
+++ Myth.java
@@ -0,0 +1,11 @@
+class Trial{
+    public static void main(String[] args) {
+        System.out.print("hello World");
+    }
+}
+
+/**
+ if file name is Main.java
+ then public class name should be also same as file name --> Main 
+ but we can create  other classes with different name in same file 
+ */
